dad409ddfe03d880b6227cb3b78771cff01659e9
Harsh <nisar.harsh@gmail.com>
2014-03-20 12:26:58 +0530

    err

---
+++ sentence.py
@@ -0,0 +1,17 @@
+import nltk
+
+eng = open('two bullocks','rb')
+print type(eng)
+count = 0
+
+sentences = []
+text_eng = eng.read()
+
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+sentences = (sent_detector.tokenize(text_eng.strip()))
+
+for sentence in sentences:
+	print sentence
+	count = count + 1
+
+print count
